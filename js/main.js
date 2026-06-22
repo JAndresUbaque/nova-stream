622df80c7dc7e30a8b658d7d2b3413c304a7cd29
JAndresUbaque <jorge.ubaque55@gmail.com>
2026-06-22 17:37:34 -0500

    Agregar archivos css  y JS base y conectado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//  Js Vanilla 
+
+console.log("🚀 NovaStream JS cargado correctamente!");
+
+document.addEventListener(`DOMContentLoaded`, () => {
+    console.log("✅ DOM completamente cargado");
+}
+)
